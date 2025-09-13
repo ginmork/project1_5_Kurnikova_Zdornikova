03e8ce0ea26a70e3bebe460d61fbb69584621892
Курникова Милана Станиславовна <1899-23@std.pkgh.edu>
2025-09-13 14:19:13 +0300

    №5

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,24 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    println("Введите целое число N и основание степени X: ")
+
+    var n = readln().toDouble()
+    var x = readln().toDouble()
+    var y = Math.log(n) / Math.log(x)
+    var result = Math.pow(x, y)
+
+    if (y % 1 == 0.0) {
+        println("Целочисленный показатель = ${y.toInt()}")
+    } else {
+        println("Целочисленный показатель не существует")
+        return
+    }
+
+    if (result.toString().endsWith(".0")) {
+        println("Ответ: ${result.toInt()}")
+    } else {
+        println("Ответ: $result")
+    }
+
+
 }
